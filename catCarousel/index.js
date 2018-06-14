a597d2da4893f888b57ba1f6d3949826d246f081
Richard Tom <tomrichard14@gmail.com>
2018-06-14 09:05:44 -0700

    cat caraosel finished

---
+++ catCarousel/index.js
@@ -0,0 +1,10 @@
+'use strict';
+function handlesClicks() {
+  $('.thumbnail').click(function(event) {
+    const imgSrc = $(this).find('img').attr('src');
+    const imgAlt = $(this).find('img').attr('alt');
+    $('.hero img').attr('src',imgSrc).attr('alt', imgAlt);
+  })
+}
+
+$(handlesClicks);
