4c773059a17443e35d1ada62f0a84da72907c9e2
Richard Tom <tomrichard14@gmail.com>
2018-06-14 10:02:11 -0700

    organized folders

--- catCarousel/index.js
+++ catCarousel/index.js
@@ -4,7 +4,7 @@ function handlesClicks() {
     const imgSrc = $(this).find('img').attr('src');
     const imgAlt = $(this).find('img').attr('alt');
     $('.hero img').attr('src',imgSrc).attr('alt', imgAlt);
-  })
+  });
 }
 
 $(handlesClicks);
